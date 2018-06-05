44394af5c722123d1ba26bd9b8ff700ccc18e000
jmpainter <jpainter4@gmail.com>
2018-06-05 10:24:59 -0700

    added tests for /recipes endpoint

--- test/test-server.js
+++ test/test-server.js
@@ -142,3 +142,80 @@ describe('Shopping List', function() {
       });
   });
 });
+
+describe('Recipes', function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list recipes on GET', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.above(0);
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.have.all.keys('id', 'name', 'ingredients');
+        });
+      });
+  });
+
+  it('should add and item on POST', function() {
+    const newItem = {
+      name: 'Green Salad',
+      ingredients: ['Broccoli, kale, lettuce']
+    };
+    return chai.request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'name', 'ingredients');
+        expect(res.body).to.not.equal(null);
+        expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
+      });
+  });
+
+  it('should update a recipe on PUT', function() {
+    const updateData = {
+      name: 'new name',
+      ingredients: ['new item 1', 'new item 2']
+    };
+
+    return chai.request(app)
+      .get('/recipes')
+      .then(function (res) {
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData)
+      })
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.deep.equal(updateData);
+      });
+  });
+
+  it('should delete items on DELETE', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+      });
+  });
+});
